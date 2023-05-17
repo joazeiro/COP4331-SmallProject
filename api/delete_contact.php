e37637ac8245b815e4e6bfd00f692de0b151adee
Maria Gomes <maria.gomes@datto.lan>
2023-05-17 08:06:45 -0400

    added initial boilerplate

---
+++ api/delete_contact.php
@@ -0,0 +1,2 @@
+// this will be used to permanently remove a contact from our list
+// remember to add the dialog box confirming that the user wants to delete it 
---
+++ api/search_contact.php
@@ -0,0 +1,2 @@
+// the searching will be implemented here
+// remember to implement first and last name search
