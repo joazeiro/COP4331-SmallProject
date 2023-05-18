ee54264e547b6d1b101a80856268b03657d333d9
Maria Gomes <maria.gomes@datto.lan>
2023-05-18 17:11:07 -0400

    added things to the login

--- api/delete_contact.php
+++ api/delete_contact.php
@@ -1,2 +1,31 @@
-// this will be used to permanently remove a contact from our list
-// remember to add the dialog box confirming that the user wants to delete it 
+<?php
+    // this will be used to permanently remove a contact from our list
+    // remember to add the dialog box confirming that the user wants to delete it 
+
+    /* ALERT BOX EXAMPLE FOR FUTURE REFERENCE
+
+    <?php
+        function_alert("We welcome the New World");
+
+        function function_alert($msg) {
+            echo "<script type='text/javascript'>alert('$msg');</script>";
+        }
+    ?>
+
+    <?php
+        function  createConfirmationmbox(){
+            echo '<script type="text/javascript"> ';
+            echo ' function openulr(newurl) {';
+            echo '  if (confirm("Are you sure you want to open new URL")) {';
+            echo '    document.location = newurl;';
+            echo '  }';
+            echo '}';
+            echo '</script>';
+        }
+    ?>
+    <?php
+        createConfirmationmbox();
+    ?>
+    */
+
+?>
--- api/sign_up.php
+++ api/sign_up.php
@@ -1,2 +1,6 @@
-// remeber that when the user signs up we need to check if there are no other users
-// with that same email in our database
+<?php
+    // remeber that when the user signs up we need to check if there are no other users
+    // with that same email in our database
+
+
+?>
