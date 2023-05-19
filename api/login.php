fdf7a3a0b91fc0eacfe4ac685eadac5df0d2d62a
Maria Gomes <maria.gomes@datto.lan>
2023-05-19 16:40:32 -0400

    finish login backend

--- api/login.php
+++ api/login.php
@@ -14,6 +14,26 @@
                 // check if user exists in the database
                 $query_user = "SELECT ID, FirstName, LastName FROM users where Login='" . htmlspecialchars($data["login"]) . "' and Password='" . htmlspecialchars($data["password"]) . "';";
                 $query_result = $connect_db->query($query_user);
+
+                if($query_result->num_rows > 0){
+                    $row = $query_result->fetch_assoc();
+                    $first_name = $row["FirstName"];
+                    $last_name = $row["LastName"];
+                    $id = $row["ID"];
+
+                    $body_json = {
+                            "id": ' . $id . ',
+                            "firstName": ' . $first_name . ',
+                            "lastName": ' . $last_name . ',
+                            "error": ''
+
+                    }
+                    header('Content-type: application/json');
+                    echo $body_json;
+
+                    $update_login_info = "UPDATE users SET DateLastLoggedIn = CURRENT_TIMESTAMP WHERE ID =" . $id . ";";
+                    $query_result = $connect_db->query($update_login_info);
+                }
         }
 
         else{
@@ -24,6 +44,12 @@
             }
             header('Content-type: application/json');
             echo $body_json;
+        }
+
+        function get_info(){
+
+            return json_decode(file_get_contents('php://input'), true);
+
         }
 
 
